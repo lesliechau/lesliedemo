6a17ee32d8c1b591ffdcfa3f888f27764aaba86b
king <king@kings-MacBook-Pro.local>
2017-02-06 12:44:56 -0600

    fix fragment notation

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 
 import PackageDescription
 
-// let applicationName = "<% applicationName %>"
+// let applicationName = "{{applicationName}}"
 let applicationName = "Application"
 
-// let applicationServerName =  "<% applicationName %>Server"
+// let applicationServerName =  "{{applicationName}}Server"
 let applicationServerName = "Server"
 
 let package = Package(
@@ -17,29 +17,19 @@ let package = Package(
         
         .Package(url: "https://github.com/IBM-Swift/Kitura.git",             majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",       majorVersion: 1),
-        .Package(url: "git@github.ibm.com:IBM-Swift/Configuration.git",      majorVersion: 0)
+        .Package(url: "git@github.ibm.com:IBM-Swift/Configuration.git",      majorVersion: 0),
 
-        // <%= configuration/bluemix/importDependency %>,
-        
-        // <%= configuration/standard/importDependency %>,
-        
-        // <%= data/cloudant/importDependency %>,
-        
-        // <%= data/mongodb/importDependency %>,
-        
-        // <%= data/redis/importDependency %>,
-        
-        // <%= data/postgresql/importDependency %>,
-        
-        // <%= middleware/profiling/importDependency %>,
-        
-        // <%= auth/mca/importDependency %>,
-        
-        // <%= auth/facebook/importDependency %>,
-        
-        // <%= auth/google/importDependency %>,
-        
-        // <%= auth/http/importDependency %>
+        // {{configuration/bluemix/importDependency}}
+        // {{configuration/standard/importDependency}}
+        // {{data/cloudant/importDependency}}
+        // {{data/mongodb/importDependency}}
+        // {{data/redis/importDependency}}
+        // {{data/postgresql/importDependency}}
+        // {{middleware/profiling/importDependency}}
+        // {{auth/mca/importDependency}}
+        // {{auth/facebook/importDependency}}
+        // {{auth/google/importDependency}}
+        // {{auth/http/importDependency}}
         
     ],
     exclude: []
